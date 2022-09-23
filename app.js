f668846fed5c5cf6c8ec2feb89952e3f63e92595
Naomi-Vega <97486808+Naomi-Vega@users.noreply.github.com>
2022-09-23 10:05:27 -0400

    added function for shortUrl

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
+var fs = require("fs");
 const app = express ();
+app.use(express.urlencoded());
+app.use(express.json());
+
 
 app.set("view engine", "ejs")
 
@@ -20,7 +24,39 @@ app.get("/myurl", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-    res.render("urls");
+    var data = fs.readFileSync("./models/urls.json")
+    var newData = JSON.parse(data)
+    newData = Object.values(newData)
+    console.log(newData)
+    res.render("urls", {
+        newData
+    });
+});
+
+app.post("/urls", (req, res) => {
+    console.log("url is created");
+    function makeid(length) {
+        var result           = '';
+        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+          result += characters.charAt(Math.floor(Math.random() * 
+     charactersLength));
+       }
+       return result;
+    }
+    
+    var shortUrl = makeid(6)
+    var longUrl = req.body.url
+    var data = fs.readFileSync("./models/urls.json")
+    var newData = JSON.parse(data)
+    newData[shortUrl] = {
+        shortUrl, longUrl
+    }
+    fs.writeFileSync("./models/urls.json", JSON.stringify(newData))
+
+    res.redirect("/urls")
+    
 });
 
 app.listen(8001, () => console.log("server running 8001"));
