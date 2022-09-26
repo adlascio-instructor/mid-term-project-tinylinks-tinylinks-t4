ae8c65b0f1119a0b2c7ae077a581035d86ccd407
Jorge Ruiz <97487049+jorgeruizm94@users.noreply.github.com>
2022-09-26 12:13:18 -0700

    small changes/ fail to complete

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,13 +8,13 @@ const { v4: uuidv4 } = require('uuid');
 
 routes.get("/login", (req, res) => {
     res.render("login", {
-        message:""
+        message:"Log in succesful"
     });
 });
 
 routes.get("/register", (req, res) => {
     res.render("register", {
-        message:""
+        message:"Register succesful"
     });
 });
 
